7777283e45d83aa12e92f8bd58b35e753dc8c53c
Andrey <lislmaka@>
2025-11-30 04:31:36 +0300

    Создал view/ListCreateAPIView для apps/testapp

--- services/backend/apps/testapp/views.py
+++ services/backend/apps/testapp/views.py
@@ -1,1 +1,14 @@
 """Docstring for services.backend.apps.testapp.views."""
+
+from rest_framework import generics
+
+from apps.testapp.models import Project
+
+from .serializers import ProjectSerializer
+
+
+class ProjectList(generics.ListCreateAPIView):
+    """Docstring for ProjectList."""
+
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
