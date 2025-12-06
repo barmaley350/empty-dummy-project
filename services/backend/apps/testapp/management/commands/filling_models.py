ab5e9b8fc303d3cba5a5eae3f6ca7e872e85771a
Andrey <lsmart@atomicmail.io>
2025-12-06 07:27:34 +0300

    Создал команду для заполнения модели Project

---
+++ services/backend/apps/testapp/management/commands/filling_models.py
@@ -0,0 +1,58 @@
+"""Docstring for services.backend.apps.testapp.management.commands.sample."""
+
+from django.core.management.base import BaseCommand
+
+from apps.testapp.factories import ProjectFactory
+from apps.testapp.models import Project
+
+
+class Command(BaseCommand):
+    """Docstring for Command."""
+
+    help = "Заполнение моделей приложения apps/testapp фейковыми данными"
+
+    def add_arguments(self, parser) -> None:  # noqa: ANN001
+        """Docstring for add_arguments.
+
+        :param self: Description
+        :param parser: Description
+        """
+        parser.add_argument(
+            "--clear",
+            action="store_true",
+            help="Очисть все данные перед заполнением (default=False)",
+        )
+
+        parser.add_argument(
+            "--count",
+            type=int,
+            default=100,
+            help="Количество записей для создания (default=100)",
+        )
+
+    def filling_project_model(self, *args, **options) -> int | None:  # noqa: ANN002, ANN003, ARG002
+        """Docstring for filling_project_model.
+
+        :param self: Description
+        """
+        count = options["count"]
+        if options["clear"]:
+            Project.objects.all().delete()
+
+        ProjectFactory.create_batch(count)
+        return Project.objects.count()
+
+    def handle(self, *args, **options) -> None:  # noqa: ANN002, ANN003
+        """Docstring for handle.
+
+        :param self: Description
+        :param args: Description
+        :param options: Description
+        """
+        count = self.filling_project_model(*args, **options)
+        self.stdout.write(
+            self.style.SUCCESS("Создание записей в моделе Project завершено")
+        )
+        self.stdout.write(
+            self.style.SUCCESS(f"Кол-во записей в моделе Project = {count}")
+        )
