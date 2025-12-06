883f356b1ef8a6cd9e10d7456abef2f467c5ce50
Andrey <lsmart@atomicmail.io>
2025-12-06 10:30:19 +0300

    Изменил логику работы скрипта run_all.sh

--- files/scripts/run_all.sh
+++ files/scripts/run_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+
 SCRIPT_DIR="$(dirname "$0")"
 cd $SCRIPT_DIR
 cd ../../
@@ -10,50 +17,116 @@ BASE_DIR=$(pwd)
 PATH_TO_BACKEND="/services/backend"
 PATH_TO_BACKEND_DOCS="/services/backend/docs"
 
+print_text_success() {
+    echo -e "${GREEN} \u2714 $1${NC}"
+}
+
+print_text_error() {
+    echo -e "${RED} \u2718 $1${NC}"
+}
+
+line_output() {
+    for i in {1..70}; do echo -en "\u2501"; done
+    echo -e ""
+}
+
+print_text_block() {
+    case $1 in
+        success)
+            line_output
+            print_text_success "$2"
+            line_output
+            ;;
+        error)
+            line_output
+            print_text_error "$2"
+            line_output
+            ;;
+    esac
+}
 
 # Generate sphinx docs for backend/django
-cd $BASE_DIR$PATH_TO_BACKEND_DOCS
-pipenv run make html
+generate_sphinx_docs() {
+    cd $BASE_DIR$PATH_TO_BACKEND_DOCS
+    pipenv run make html
 
-if [ $? -ne 0 ]; then
-    echo "Генерация документации Sphinx...Error "
-    echo ""
-    exit $?
-fi
+    if [ $? -ne 0 ]; then
+        print_text_block error "Ошибка при создании документации Sphinx"  
+        exit $?
+    fi
 
-echo "Генерация документации Sphinx...OK "
+    print_text_block success "Генерация документации Sphinx прошла успешно"
+    return 0
+}
 
-# Ruff linter backend/django
-cd $BASE_DIR$PATH_TO_BACKEND
 
-pipenv run ruff check --fix
+# Generate sphinx docs for backend/django
+generate_graph_models() {
+    cd $BASE_DIR$PATH_TO_BACKEND
+    pipenv run python3 manage.py graph_models testapp -o docs/_static/testapp.png
+    pipenv run python3 manage.py graph_models sphinx_docs -o docs/_static/sphinx_docs.png
+    pipenv run python3 manage.py graph_models -o docs/_static/all.png
 
-if [ $? -ne 0 ]; then
-    echo "ruff check...Error "
-    echo ""
-    exit $?
-fi
-echo "ruff check...OK "
+    if [ $? -ne 0 ]; then
+        print_text_block error "Генерация картинки структуры DB...Error "
+        exit $?
+    fi
+    cp docs/_static/* $BASE_DIR/files/img/graph_models
 
-# Ruff formater backend/django
-pipenv run ruff format
+    print_text_block success "Генерация картинки структуры DB...OK "
+    return 0
+}
 
-if [ $? -ne 0 ]; then
-    echo "ruff format...Error "
-    echo ""
-    exit $?
-fi
-echo "ruff format...OK "
+start_ruff_check() {
+    cd $BASE_DIR$PATH_TO_BACKEND
 
-# pytest backend/django
-docker exec -it ${PROJECT_NAME}-service.backend-1 pipenv run pytest
+    pipenv run ruff check --fix
 
-if [ $? -ne 0 ]; then
-    echo "pytest...Error "
-    echo ""
-    exit $?
-fi
-echo "pytest...OK "
+    if [ $? -ne 0 ]; then
+        print_text_block error "ruff check...Error "
+        exit $?
+    fi
+    print_text_block success "ruff check...OK "
+    return 0
+}
 
-cd $BASE_DIR
-git status
+start_ruff_format() {
+    # Ruff formater backend/django
+    pipenv run ruff format
+
+    if [ $? -ne 0 ]; then
+        print_text_block error "ruff format...Error "
+        exit $?
+    fi
+    print_text_block success "ruff format...OK "
+}
+
+
+start_tests() {
+    # pytest backend/django
+    docker exec -it ${PROJECT_NAME}-service.backend-1 pipenv run pytest
+
+    if [ $? -ne 0 ]; then
+        print_text_block error "pytest...Error "
+        exit $?
+    fi
+    print_text_block success "pytest...OK "
+}
+
+start_git() {
+    cd $BASE_DIR
+    git status 
+}
+
+
+
+main() {
+    generate_sphinx_docs
+    generate_graph_models
+    start_ruff_check
+    start_ruff_format
+    start_tests
+    start_git
+}
+
+main "$@"
