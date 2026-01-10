0786ced00b566293e80df9d7c4166f72f7ea548b
Andrey <246589417+barmaley350@users.noreply.github.com>
2026-01-10 13:11:27 +0300

    Добавил модели в fastapi из django

--- services/fastapi/app/main.py
+++ services/fastapi/app/main.py
@@ -1,13 +1,25 @@
+"""Docstring для services.fastapi.app.main."""
+
 from fastapi import FastAPI
 
 app = FastAPI(title="My FastAPI App", version="1.0.0")
 
 
 @app.get("/")
-def read_root():
+def read_root() -> None:
+    """_summary_.
+
+    :return: _description_
+    :rtype: _type_
+    """
     return {"message": "Hello from FastAPI in Docker!"}
 
 
 @app.get("/health")
-def health_check():
+def health_check() -> dict:
+    """_summary_.
+
+    :return: _description_
+    :rtype: dict
+    """
     return {"status": "healthy"}
